93990201f90f343dd013f129efcf13840a8bca29
Tkwok7 <tkwok7@gatech.edu>
2024-05-06 10:21:01 -0400

    Initial Commit

---
+++ SKRSkeletonAppIOS/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SKRSkeletonAppIOS
+//
+//  Created by Tyler Kwok on 5/6/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SKRSkeletonAppIOS/SKRSkeletonAppIOSApp.swift
@@ -0,0 +1,17 @@
+//
+//  SKRSkeletonAppIOSApp.swift
+//  SKRSkeletonAppIOS
+//
+//  Created by Tyler Kwok on 5/6/24.
+//
+
+import SwiftUI
+
+@main
+struct SKRSkeletonAppIOSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
